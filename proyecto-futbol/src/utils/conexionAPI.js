568acba4b7b159440d2afe98f480a83f80150fd5
Alejandro Ferreiro <dg.alef@gmail.com>
2024-07-10 18:17:51 -0300

    Sumo segunda API key

--- proyecto-futbol/src/utils/conexionAPI.js
+++ proyecto-futbol/src/utils/conexionAPI.js
@@ -3,7 +3,7 @@ const API ="https://v3.football.api-sports.io"
 export const getUrl = (path) =>{
     return fetch (API+path,{
         headers: {
-            'x-rapidapi-key': 'e802866635be846647badd14e1819ca5',   // Esta API key tiene un limite diario de 100 consultas
+            'x-rapidapi-key': '6c3618a24b636f39602d1687d6621a68',   // Esta API key tiene un limite diario de 100 consultas
             'x-rapidapi-host': 'v3.football.api-sports.io'
           }
     }).then((results)=>results.json())
